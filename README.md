5dfd87a791bd8380228dc94cbbd9adbf4551832d
Hunter Liu <wantedliu@gmail.com>
2016-04-19 00:12:34 +0800

    First commit in 160418

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # PythonChallenge
 :key: Python3 solutions of puzzles from pythonchallenge.com
-> Have fun with these puzzles :) , use Python3 definitely.
+:unlock: Have fun with these puzzles , use Python3 definitely.:yum:
 
 |Level|Add Date|Need to Know|
-|:----|--------|------------|
+|:---:|--------|------------|
 |[01](level_01.py)|2016/04/18|The usage of maketrans, it's no longer supported by string in Python3, just use str.maketrans|
--- level_01.py
+++ level_01.py
@@ -1,11 +1,17 @@
+#Get the mission HTML data with urlopen.
 from urllib.request import urlopen
 CHALLENGE_URL = 'http://www.pythonchallenge.com/pc/def/map.html'
 text = urlopen(CHALLENGE_URL).read()
+
 #Create a translation table to shift each lower case ASCII byte by 2 places cyclically.
 lower = bytes(i for i in range(ord('a'), ord('z') + 1))
 table = bytes.maketrans(lower, lower[2:] + lower[:2])
+
+#Find the enciphered text.
 start = text.index(b'g fmnc')
 stop = start + text[start:].index(b'\n')
 enc = text[start:stop]
+
+#Translate enciphered text.
 print(enc.translate(table))
 print(b'map'.translate(table))
